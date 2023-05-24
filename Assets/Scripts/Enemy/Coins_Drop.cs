5a149a6a9ecf0cd21e069110e7cf5a87819a4212
Matszak <mtracz2@edu.cdv.pl>
2023-05-24 14:29:14 +0200

    Poprawiona tekstura coina

--- Assets/Scripts/Enemy/Coins_Drop.cs
+++ Assets/Scripts/Enemy/Coins_Drop.cs
@@ -21,11 +21,12 @@ public class Coins_Drop : MonoBehaviour
         if (!isDead)
         {
             isDead = true;
+            coinCount = Random.Range(1, 5);
 
             for (int i = 0; i < coinCount; i++)
             {
                 GameObject coin = Instantiate(coinPrefab, transform.position, quaternion.identity);
-                coin.transform.position += new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+                coin.transform.position += new Vector3(Random.Range(-2f, 2f), 0, Random.Range(-5f, 5f));
                 CoinScript = coin.GetComponent<Coin>();
                 if(CoinScript != null)
                     CoinScript.isSpawn = true;
